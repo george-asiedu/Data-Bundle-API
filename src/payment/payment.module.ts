cdf34486f3a85b73f333fcb52f526288ef3d5974
george-asiedu <asiedug41@gmail.com>
2026-05-20 20:06:12 +0000

    repositories added to exports

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wallet } from './entities/wallet.entity';
+import { Transaction } from './entities/transactions.entity';
+import { WalletRepository } from './repositories/wallet.repository';
+import { TransactionRepository } from './repositories/transaction.repository';
 
 @Module({
-  imports: [AuthModule],
-  providers: [PaymentService],
+  imports: [
+    TypeOrmModule.forFeature([Wallet, Transaction]),
+    forwardRef(() => AuthModule),
+  ],
+  providers: [PaymentService, WalletRepository, TransactionRepository],
   controllers: [PaymentController],
-  exports: [PaymentService],
+  exports: [PaymentService, WalletRepository, TransactionRepository],
 })
 export class PaymentModule {}
