4a082156ba2420273eb8fb4a397f6c364ed1bac6
george-asiedu <asiedug41@gmail.com>
2026-05-20 17:41:26 +0000

    removed role implementations

--- src/auth/guards/google-oauth.guard.ts
+++ src/auth/guards/google-oauth.guard.ts
@@ -1,15 +1,5 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import type { Request } from 'express';
 
 @Injectable()
-export class GoogleOAuthGuard extends AuthGuard('google') {
-  getAuthenticateOptions(context: ExecutionContext) {
-    const req = context.switchToHttp().getRequest<Request>();
-    const role = req.query.role as string;
-    return {
-      scope: ['email', 'profile'],
-      state: role,
-    };
-  }
-}
+export class GoogleOAuthGuard extends AuthGuard('google') {}
