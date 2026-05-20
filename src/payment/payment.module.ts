cad01175a058ea471a04fb13afb7c1a0519d199d
george-asiedu <asiedug41@gmail.com>
2026-05-20 16:53:43 +0000

    auth module added to imports array

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [PaymentService],
   controllers: [PaymentController],
   exports: [PaymentService],
