5593da0834022f1e2992f152a5d71d6377a670a0
george-asiedu <asiedug41@gmail.com>
2026-05-30 05:45:44 +0000

    updated module

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -2,11 +2,17 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './entities/subscription.entity';
 import { AuthModule } from '../auth/auth.module';
+import { SubscriptionsRepository } from './repository/subscription.repository';
+import { SubscriptionController } from './subscription.controller';
+import { SubscriptionService } from './subscription.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Subscription]),
     forwardRef(() => AuthModule),
   ],
+  providers: [SubscriptionsRepository, SubscriptionService],
+  controllers: [SubscriptionController],
+  exports: [SubscriptionService],
 })
 export class SubscriptionModule {}
