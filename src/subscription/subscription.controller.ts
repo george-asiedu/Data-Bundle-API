af9920a69d9e523d80c8b13defd44430e1d02d63
george-asiedu <asiedug41@gmail.com>
2026-05-31 11:37:41 +0000

    added guards

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '../auth/entities/user.entity';
 import { CurrentUser } from '../shared/decorators/current-user.decorator';
 import { SubscriptionService } from './subscription.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
+@UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('subscription')
 export class SubscriptionController {
   constructor(private readonly _subService: SubscriptionService) {}
 
+  @ApiOperation({ summary: 'Gets the subscription status of a user' })
   @Get('status')
   async getStatus(@CurrentUser() user: User) {
     const isActive = await this._subService.hasAccess(user.id);
