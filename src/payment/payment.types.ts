79bcf59d9b65fc41fec985dc2c40cf3b3218da84
george-asiedu <asiedug41@gmail.com>
2026-06-03 13:25:53 +0000

    added payment model interfaces

--- src/payment/payment.types.ts
+++ src/payment/payment.types.ts
@@ -25,3 +25,40 @@ export interface PaymentSuccessPayload {
   purpose: string;
   reference: string;
 }
+
+export type PaystackVerifyResponse = {
+  status: boolean;
+  message: string;
+  data: {
+    status: string;
+    reference: string;
+    amount: number;
+    metadata?: {
+      userId?: string;
+      purpose?: TransactionPurpose;
+    };
+    customer?: {
+      email?: string;
+      first_name?: string;
+    };
+  };
+};
+
+export interface PaystackCreateSubaccountResponse {
+  status: boolean;
+  message: string;
+  data: {
+    subaccount_code: string;
+  };
+}
+
+export interface PaystackBankResolutionResponse {
+  status: boolean;
+  message: string;
+  data: {
+    account_name: string;
+    account_number: string;
+    bank_id: number;
+    bank_name: string;
+  };
+}
