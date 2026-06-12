6a310d7e9e65703be1ae4c9a7ef4894de77ee687
george-asiedu <asiedug41@gmail.com>
2026-06-12 21:26:07 +0000

    added prompt to google oauth guard

--- src/auth/guards/google-oauth.guard.ts
+++ src/auth/guards/google-oauth.guard.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleOAuthGuard extends AuthGuard('google') {}
+export class GoogleOAuthGuard extends AuthGuard('google') {
+  getAuthenticateOptions() {
+    return { prompt: 'select_account' };
+  }
+}
