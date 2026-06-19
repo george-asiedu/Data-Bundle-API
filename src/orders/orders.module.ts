dbadbea6bbc34a26244542b36cbc7c45e5cdacd0
george-asiedu <asiedug41@gmail.com>
2026-06-19 16:20:12 +0000

    orders module defined

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderRepository } from './repositories/order.repository';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { OrdersPoller } from './orders.poller';
+import { VerdeaccessProvider } from './suppliers/verdeaccess.provider';
+import { SupplierRegistry } from './suppliers/supplier.registry';
+import { SUPPLIER_PROVIDERS } from './suppliers/supplier.provider';
+import { AuthModule } from '../auth/auth.module';
+import { PaymentModule } from '../payment/payment.module';
+import { PackagesModule } from '../packages/packages.module';
+import { AuditModule } from '../audit/audit.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+    AuthModule,
+    PaymentModule,
+    PackagesModule,
+    AuditModule,
+  ],
+  providers: [
+    OrderRepository,
+    OrdersService,
+    OrdersPoller,
+    VerdeaccessProvider,
+    SupplierRegistry,
+    {
+      // The set of supplier integrations the registry resolves from.
+      provide: SUPPLIER_PROVIDERS,
+      useFactory: (verde: VerdeaccessProvider) => [verde],
+      inject: [VerdeaccessProvider],
+    },
+  ],
+  controllers: [OrdersController],
+  exports: [OrdersService, OrderRepository],
+})
+export class OrdersModule {}
